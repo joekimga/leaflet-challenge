c67c20026815625e20ccb3ea7aa068d4b7b0a1b7
Joseph Kim <joekimga@gmail.com>
2021-04-20 18:42:47 -0400

    graymap on DOM

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -5,7 +5,7 @@ console.log("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.
 var grayscalemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox/light-v10",
+    id: "light-v10",
     accessToken: API_KEY
     });
 
